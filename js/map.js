f29dcb35bb8278f30e6b08bb05ec5c96f94e0117
Roger Yu-Hsiang Lo <b13472003@gmail.com>
2020-11-05 15:14:46 -0800

    Added project backbone structure

---
+++ js/map.js
@@ -0,0 +1,27 @@
+class Map {
+
+    constructor(_config) {
+        this.config = {
+            parentElement: _config.parentElement,
+            height: _config.height || 300,
+            margin: {top:10, botton: 30, right: 10, left: 30}
+        };
+
+        this.initVis();
+    }
+
+    initVis() {
+        let vis = this;
+
+    }
+
+    update() {
+        let vis = this;
+        vis.render();
+    }
+
+    render() {
+        let vis = this;
+
+    }
+}
